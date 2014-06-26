715e50a0e7aca2f6ea8b504db30ef1953778e64f
thernandez117 <thernandez117@aol.com>
2014-06-25 20:06:43 -0700

    Signed-off-by: thernandez117 <thernandez117@aol.com>

---
+++ Hmwk/Hello_World/main.cpp
@@ -0,0 +1,25 @@
+/*
+* File: main.cpp
+* Author: Thomas Hernandez
+* Created on June 23, 2014, 7:48 PM
+* Purpose: First Program Example
+*/
+
+//System Level Libraries
+#include <iostream>//Input Output Library
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+//Output a string
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
